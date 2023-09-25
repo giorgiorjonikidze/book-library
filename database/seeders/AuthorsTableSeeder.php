eacf75c5809b88478e6f89db27f3c9ffab71173d
giorgi orjonikidze <gioorjo123@gmail.com>
2023-09-25 23:10:17 +0400

    feat: create factories and seeders

---
+++ database/seeders/AuthorsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Author;
+
+class AuthorsTableSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		Author::factory(5)->create();
+	}
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AuthorBookSeeder;
+use Database\Seeders\BooksTableSeeder;
+use Database\Seeders\AuthorsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,17 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run(): void
 	{
-		// \App\Models\User::factory(10)->create();
+		\App\Models\User::factory(10)->create();
 
-		// \App\Models\User::factory()->create([
-		//     'name' => 'Test User',
-		//     'email' => 'test@example.com',
-		// ]);
+		\App\Models\User::factory()->create([
+		    'name' => 'Test User',
+		    'email' => 'test@example.com',
+		]);
+
+		$this->call([
+            AuthorsTableSeeder::class,
+            BooksTableSeeder::class,
+			AuthorBookSeeder::class,
+        ]);
 	}
 }
