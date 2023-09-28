9f9baacffb7aab9846842e8a8f6aeb8792fff6c7
giorgi orjonikidze <gioorjo123@gmail.com>
2023-09-28 10:49:29 +0400

    chore: add errorlabel

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,6 +20,7 @@
             @error('password')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
             @enderror
+        <p class="mt-2 text-[8px] text-red-600 dark:text-red-500">make sure you have created user.</p>
         </div>
         <button type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log
